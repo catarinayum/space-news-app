49ab5f19a6eecf39a75b94cfa44ccf74ce1319ec
catarinayum <catarina.yum@gmail.com>
2024-03-14 11:42:01 +0000

    fixed card image sizes, added enter key for input

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -10,11 +10,18 @@ export function ArticleList({ articles, loading }) {
       {
         // eslint-disable-next-line react/prop-types
         articles.map((article) => (
-          <li key={article.id} className="article">
+          <li key={article.id} className="article rounded">
             <a href={article.url} target="_blank">
+              {" "}
+              <div className="img-container">
+                <img
+                  src={article.image_url}
+                  className="article-img img-fluid rounded"
+                ></img>{" "}
+              </div>
               <h4>{article.title}</h4>
-              <img src={article.image_url} className="article-img"></img>
               <p>{article.summary}</p>
+              <p className="read-more">Read more...</p>
             </a>
           </li>
         ))
